a564f38b86723826bd74c50af1e4984db05224c3
GamesGG <gamesgg567@gmail.com>
2025-08-20 18:31:38 -0700

    Update Buttons.lua

--- Buttons.lua
+++ Buttons.lua
@@ -3,7 +3,7 @@
 local PlayerService = game:GetService("Players")
 local LocalPlayer = PlayerService.LocalPlayer
 local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
-local Humanoid = Character:WaitForChild("Humanoid")
+local Humanoid : Humanoid = Character:WaitForChild("Humanoid")
 local HRP = Character:WaitForChild("HumanoidRootPart")
 local GameGUI = script.Parent.Parent.Parent
 local SeedUI = LocalPlayer.PlayerGui:WaitForChild("GameGUI")
@@ -13,22 +13,23 @@ GameGUI.Parent = LocalPlayer.PlayerGui
 
 local LoadWorld = script.Parent
 local WaterToggle = script.Parent.Parent["Water Toggle"]
-local Seed = script.Parent.Parent.Seed
+local SeedInput = script.Parent.Parent.Seed
 local GenFlat = script.Parent.Parent["Generation Flatness"]
 local WorldSize = script.Parent.Parent["World Size"]
 local GUI = script.Parent.Parent.Parent
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local WorldLoad = ReplicatedStorage:WaitForChild("WorldLoad")
+local LoadWorldFunction = ReplicatedStorage:WaitForChild("LoadWorld")
+local WorldLoadedFunction = ReplicatedStorage:WaitForChild("WorldLoaded")
 
 local WaterEnabled = true
 
-
 local SpawnPoint = game.Workspace:WaitForChild("SpawnLocation")
-
+local TweenService = game:GetService("TweenService")
 
 HRP.Anchored = true --Player Can't move until world is generated
 
+local Seed = 0 
 WaterToggle.MouseButton1Down:Connect(function()
 	WaterEnabled = not WaterEnabled
 
@@ -41,18 +42,19 @@ WaterToggle.MouseButton1Down:Connect(function()
 	end
 end)
 
+local UITweenInfo = TweenInfo.new(1, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
 LoadWorld.MouseButton1Down:Connect(function()
+	TweenService:Create(script.Parent.Parent, UITweenInfo, {Position = UDim2.new(0.5, 0, 2, 0)}):Play()
+	
+	task.wait(0.7)
+
 	GUI.Enabled = false
+	LoadWorldFunction:FireServer(WaterEnabled, SeedInput.Text, GenFlat.Text, WorldSize.Text)
+end)
 
-	local AccualSeed = WorldLoad:InvokeServer(WaterEnabled, Seed.Text, GenFlat.Text, WorldSize.Text)
-
-	print("Whyyyyyyyyy")
-	if AccualSeed then
-		SeedUI.Seed.Text = "Seed: " .. AccualSeed
-		HRP.CFrame = SpawnPoint.CFrame
-		HRP.Anchored = false
-		
-	end
-	
-	
+WorldLoadedFunction.OnClientEvent:Connect(function(NewSeed) 
+	SeedUI.Seed.Text = "Seed: " .. NewSeed
+	HRP.Anchored = false
+	HRP.CFrame = SpawnPoint.CFrame
+	NewSeed = Seed
 end)
